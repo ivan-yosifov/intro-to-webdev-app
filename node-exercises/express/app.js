2faaa288cd0496d4227cd00c0b2bc004663371d8
Ivan Yosifov <ivan_yosifov@yahoo.com>
2015-07-27 18:37:11 +0300

    all

--- node-exercises/express/app.js
+++ node-exercises/express/app.js
@@ -4,6 +4,9 @@ var app = express();
 app.get('/hello.txt', function(req, res){
   res.send('Hello World');
 });
+app.get('/bye.txt', function(req, res){
+  res.send('Bye noob!');
+});
 
 var server = app.listen(8080, function() {
     console.log('Listening on port 8080');
